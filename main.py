f0c299e60c6ef0e07d891082d7f0565e2ca2d94d
Lindsey Kitchell <kitchell@indiana.edu>
2017-09-25 15:37:34 -0400

    update with new afq output

--- main.py
+++ main.py
@@ -49,48 +49,49 @@ all_colors = []
 print config["AFQ"]
 for file in glob.glob(config["AFQ"] + "/*.json"):
 # for file in glob.glob("tracts/*.json"):
-    with open(file) as data_file:
-        tract = json.load(data_file)
-    bundle = []
-    for i in range(len(tract['coords'])):
-        templine = np.zeros([len(tract['coords'][i][0][0]), 3])
-        templine[:, 0] = tract['coords'][i][0][0]
-        templine[:, 1] = tract['coords'][i][0][1]
-        templine[:, 2] = tract['coords'][i][0][2]
-        bundle.append(templine)
-    all_bundles.append(bundle)
-    all_colors.append(tract['color'])
-    split_name = tract['name'].split(' ')
-    imagename = '_'.join(split_name)
-
-    for d in range(len(camera_pos)):  # directions: axial, sagittal, coronal
-        renderer = window.Renderer()
-        stream_actor = actor.streamtube(bundle, colors=tract['color'],
-                                        linewidth=1)
-        renderer.set_camera(position=camera_pos[d],
-                            focal_point=focal_point[d],
-                            view_up=view_up[d])
-
-        renderer.add(stream_actor)
-        slice_actor = actor.slicer(data, affine, value_range)
-        if d == 0:
-            slice_actor.display(z=int(slice_view[0]))
-        elif d == 2:
-            slice_actor.display(y=int(slice_view[2]))
-        else:
-            slice_actor.display(x=int(slice_view[1]))
-        renderer.add(slice_actor)
-
-#        show_m = window.ShowManager(renderer, size=(800, 700))
-#        show_m.initialize()
-#        show_m.render()
-#        show_m.start()
-#        renderer.camera_info() #get location of camera
-
-        window.snapshot(renderer,
-                        fname='images/'+imagename+'_'+views[d]+'.png',
-                        size=(800, 800), offscreen=True,
-                        order_transparent=False)
+    if file != config["AFQ"]+ '/tracts.json':
+        with open(file) as data_file:
+            tract = json.load(data_file)
+        bundle = []
+        for i in range(len(tract['coords'])):
+            templine = np.zeros([len(tract['coords'][i][0][0]), 3])
+            templine[:, 0] = tract['coords'][i][0][0]
+            templine[:, 1] = tract['coords'][i][0][1]
+            templine[:, 2] = tract['coords'][i][0][2]
+            bundle.append(templine)
+        all_bundles.append(bundle)
+        all_colors.append(tract['color'])
+        split_name = tract['name'].split(' ')
+        imagename = '_'.join(split_name)
+    
+        for d in range(len(camera_pos)):  # directions: axial, sagittal, coronal
+            renderer = window.Renderer()
+            stream_actor = actor.streamtube(bundle, colors=tract['color'],
+                                            linewidth=1)
+            renderer.set_camera(position=camera_pos[d],
+                                focal_point=focal_point[d],
+                                view_up=view_up[d])
+    
+            renderer.add(stream_actor)
+            slice_actor = actor.slicer(data, affine, value_range)
+            if d == 0:
+                slice_actor.display(z=int(slice_view[0]))
+            elif d == 2:
+                slice_actor.display(y=int(slice_view[2]))
+            else:
+                slice_actor.display(x=int(slice_view[1]))
+            renderer.add(slice_actor)
+    
+    #        show_m = window.ShowManager(renderer, size=(800, 700))
+    #        show_m.initialize()
+    #        show_m.render()
+    #        show_m.start()
+    #        renderer.camera_info() #get location of camera
+    
+            window.snapshot(renderer,
+                            fname='images/'+imagename+'_'+views[d]+'.png',
+                            size=(800, 800), offscreen=True,
+                            order_transparent=False)
 
 
 for d in range(len(camera_pos)):  # directions: axial, sagittal, coronal
